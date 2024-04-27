d7e610c0e37deb8d763ba1b685621ab325d44925
unknown <templeajuzie>
2024-04-27 13:35:10 +0100

    fix: removed the dist directory

--- dist/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const userController_js_1 = require("../controllers/userController.js");
-const express_1 = require("express");
-const userRouter = (0, express_1.Router)();
-userRouter.route("/single-user/:id").get(userController_js_1.getSingleUser);
-exports.default = userRouter;
