6fa4c05072e88e77f5e1b8637ba9a490815a19a5
Awajimijan Igoni <mijanigoni@gmail.com>
2024-04-30 18:23:26 +0100

    sending the reset token in the request body together with the new password

--- src/routes/v1/authRoute.ts
+++ src/routes/v1/authRoute.ts
@@ -18,7 +18,7 @@ authRouter.route("/forgot-password").post(forgotPassord);
 authRouter
   .route("/verify/:token")
   .get(verifyToken);
-authRouter.route("/reset-password/:token").post(updatePassword);
+authRouter.route("/reset-password").post(updatePassword);
 
 //Onboarding route
 authRouter.route("/add-business").post(addBusiness);
